ad577a75bb5581f1a7abf1b664e8e30a26734e17
Andre Cardoso <andrecardosodev@gmail.com>
2015-10-19 11:38:06 -0200

    Um pouco mais sobre good citizen

--- curso-swift-alura/MyPlayground.playground/Contents.swift
+++ curso-swift-alura/MyPlayground.playground/Contents.swift
@@ -7,16 +7,44 @@ class Meal {
     
     var name: String
     var happiness: Int
+    var items = Array<Item>()
     
     init(name: String, happiness: Int) {
         self.name = name
         self.happiness = happiness
     }
     
+    func allCalories() -> Double {
+     
+        var total: Double = 0
+        
+        for i in items {
+            total += i.calories
+        }
+        
+        return total
+    }
+    
 }
 
 
+class Item {
+    
+    var name: String
+    var calories: Double
+    
+    init(name: String, calories: Double) {
+        
+        self.name = name
+        self.calories = calories
+        
+    }
+    
+}
+
 var brownie = Meal(name: "Eggplant brownie", happiness: 5)
 
+brownie.items.append(Item(name: "brownie", calories: 115))
+brownie.items.append(Item(name: "vegan cream", calories: 40))
 
-
+print(brownie.allCalories())
